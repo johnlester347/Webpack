c606057c03d510a5626a127ea9b4376d493d5237
JohnLester <jonhlesterbalucas.pamplona@gmail.com>
2021-07-01 02:57:42 +0700

    Entry, Output, Loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -14,7 +15,8 @@ module.exports = {
             {
                 test: /\.(png|jpg)$/,
                 use: [
-                    'file-loader' // npm install file-loader --save-dev
+                    'file-loader' // This loader will going to import our jpg and png into dist folder
+                    // npm install file-loader --save-dev
                 ]
             },
             {
@@ -36,7 +38,25 @@ module.exports = {
                     // npm install css-loader style-loader sass-loader --save-dev
                     // REMEMBER NA RIGHT TO LEFT YUNG PAG READ SA LOADER 
                 ]
+            },
+            {
+                test: /\.js$/, // This will look for all the files that ends with .js
+                exclude: /node_modules/, // We need to use this para hindi ma apply sa node modules yung babel loader
+                use: {
+                    loader: 'babel-loader', // This is the package that convert ES6 to ES5
+                    options: {
+                        'presets': ["@babel/preset-env"], // This will compile Ecmascript 6,7,8,9 etc down to ES5
+                        'plugins': ["@babel/plugin-proposal-class-properties"]
+                        // npm install --save-dev babel-loader@7 babel-core babel-preset-env babel-plugin-transform-class-properties
+                        // npm install --save-dev @babel/preset-env
+                    }
+                }
             }
         ]
-    }
+    },
+    plugins: [
+        new TerserPlugin()
+        // npm install --save-dev terser-webpack-plugin
+    ]
 }
+
